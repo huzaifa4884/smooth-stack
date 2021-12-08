5ef093476026e619c60051d59ef722335f2e9600
huzaifa4884 <huzaifa.af@gmail.com>
2021-12-08 14:21:13 -0500

    changes floating question

--- week1/day2/week1_day2_floating.cpp
+++ week1/day2/week1_day2_floating.cpp
@@ -9,3 +9,10 @@ int main()
 	std::cout<<a<<std::endl;
        	return 0;	
 }
+
+//so the main idea is that even after the subtraction the resulting number wont change at all
+//to meassure the precision according to IEEE 754
+// if the floating point number is 32 bit
+// lets say forexample we have number 3.5 its between 2 and 4
+// what we do is (4-2/2^23) in case of 32 bits number
+// in simple words range/mentisaa = precssion                                               
