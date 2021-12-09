27839ebf3a91cba25d15eb8b8ff1b9ac12df7e83
huzaifa4884 <huzaifa.af@gmail.com>
2021-12-09 15:24:00 -0500

    added discussion on q3 and q4

--- week1/day3/week1_day3_mixedarray.cpp
+++ week1/day3/week1_day3_mixedarray.cpp
@@ -3,25 +3,28 @@
 
 int main()
 {
-    void *ptr[3]; //always intiate with 2*n size one index for type
-    char *type[3];
+    void *ptr[3]; 		//actuall data array
+    char *type[3];		//character array to keep track of data types
     
     //below are my inputs
     int i = 12;
     float a = 13.5;
     double b = 14.98374982;
     
+    //filling type array with types
     type[0] = (char*) typeid(i).name();
     type[1] = (char*) typeid(a).name();
     type[2] = (char*) typeid(b).name();
     
     
+    //filling data into void pointer array to keep multiple types
     memcpy(ptr + 0, &i, sizeof i);
     memcpy(ptr + 1 , &a, sizeof a);
     memcpy(ptr + 2 , &b, sizeof b);
     
     
     
+    //And printing
     for (int i =0; i<3; i++)
     {
         if (*type[i] == 'i')
@@ -40,9 +43,10 @@ int main()
         }
     }
     return 0;
-    
-    
 }
 
-//yes
+//In my solution I will  YES cause i am telling data types on runtime
+//i used another character pointer array to do that to keep track of types as well
 
+//but if i want to tell the types of data from void pointer array i can never tell which type of data i am getting to make it simple i will say that there is no way to tell difference between floats and ints
+//cause in memory we only 0s and 1s
