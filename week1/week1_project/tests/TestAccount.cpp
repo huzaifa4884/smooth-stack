e459267d19dfcd5e9a71565b78e57c5f5b0e42d2
huzaifa4884 <huzaifa.af@gmail.com>
2021-12-13 19:12:54 -0500

    Test Accounts Files.cPP

---
+++ week1/week1_project/tests/TestAccount.cpp
@@ -0,0 +1,42 @@
+#include "../Account.h"
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+
+void TestDelete(string accnum , bool expected)
+{
+    Account ac;
+    if (expected == ac.deleteAccount(accnum))
+    {
+        cout<<"test passed"<<endl;
+    }
+    else
+    {
+        cout<<"test failed"<<endl;
+    }
+}
+
+void TestCreate(string fname ,string lname , string ssn , bool expected)
+{
+    Account ac;
+    if (expected == ac.createAccount(fname , lname , ssn))
+    {
+        cout<<"test passed"<<endl;
+    }
+    else
+    {
+        cout<<"test failed"<<endl;
+    }
+}
+int main()
+{
+    TestDelete("1234" , true);
+    TestDelete("123" , false);
+    TestDelete("" , false);
+
+    TestCreate("huzaifa" , "afridi" , "" , true);
+    TestCreate("huzaifa" , "" , "123-13-1234" , true);
+    TestCreate("" , "afridi" , "123-13-1234" , true);
+}
