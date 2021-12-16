3c88729360c4c9aea1b25925c1d43d5a870597ed
huzaifa4884 <huzaifa.af@gmail.com>
2021-12-16 14:33:26 -0500

    corrected binet code

--- week2/day1/binet.cpp
+++ week2/day1/binet.cpp
@@ -1,19 +1,43 @@
-#include<iostream>
-#include<cmath>
+#include <iostream>
+#include <cmath>
 
-int findIndex(int n)
+int fibonacci(int index)
 {
-    //double fibo = 2.078087 * log(n) + 1.672276;
-    //double fibo = std::sqrt(5) * log(n) + ((1-sqrt(5)/2));
-    double fibo = (log(n) + log(std::sqrt(5)))/log(sqrt(5));
- 
-    return std::round(fibo);
+    return std::round(pow((1 + sqrt(5)) / 2, index) / sqrt(5));
+}
+
+long int fibonacci(double index) 
+{
+    return std::round(pow((1 + sqrt(5)) / 2, index) / sqrt(5));
 }
 
 int main()
 {
-	int number;
-	std::cout<<"Enter the value for number    :";
-	std::cin>>number;
-	std::cout<<findIndex(number)+1<<std::endl;
+    int fibnum = 2147483647;
+    
+    int index = 2.078087 * log(fibnum) + 1.672276;  //if i pass largest integer it will give me the index to nearest fibonacci number;
+    
+    
+    fibnum = fibonacci(index);      //this calculate the largest fibonaaci number in integer range
+    
+    long int lr = fibonacci(index + 1);
+    
+
+    
+    std::cout<<index<<" , "<<fibnum<<std::endl;
+    
+    std::cout<<index + 1 << " , " <<lr << "  and this goes out of iteger range cause funtion returns int value the returned value is errored"<<std::endl;
+    
+    
+    
+    double newindex = index + 1;
+    
+    std::cout<< newindex << " , " << fibonacci(newindex) << "  this fibonacci numberis larger then integer range which is 2147483647"<<std::endl;
+
+    return 0;
 }
+
+//first i pass the largest integer to inverse binet equation which return index of nearest fibonacci to highest integer
+//then i calculate fibbonacci of that index
+//then i do 1 + index to see if the next fibonacii goea out of integer range which does
+//then create new function which return "long int" and that is correct fibonacci number but larger then highest integer
