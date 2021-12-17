29a1d1bf622a3428be6b5a2a5b222c4997ccba81
huzaifa4884 <huzaifa.af@gmail.com>
2021-12-17 03:02:52 -0500

    Fun ptrs question done

---
+++ week2/day3/funptrs.cpp
@@ -0,0 +1,51 @@
+
+#include <iostream>
+#include <cassert>
+
+int (*funpt[3])(int , int);
+
+int add(int x , int y)
+{
+    return x + y;
+}
+
+int substract(int x , int y)
+{
+    return x - y;
+}
+
+int multiply(int x , int y)
+{
+    return x * y;
+}
+
+int compute(int x , int y , char funcmask)
+{
+    funpt[0] = &add;
+    funpt[1] = &substract;
+    funpt[2] = &multiply;
+    
+    unsigned op = (unsigned) funcmask & 7;
+    switch(op)
+    {
+        case 1:
+            return add(x , y);
+        case 2:
+            return substract(x , y);
+        case 4:
+            return multiply(x , y);
+        default:
+            std::cout<<"no valid selection made";
+            return 0;
+    }
+}
+
+int main()
+{
+    std::cout<<"calling add 2 + 3  =" << compute(2 , 3 , '1')<<std::endl; //calling add
+    std::cout<<"calling add 2 - 3  ="<< compute(2 , 3 , '2')<<std::endl; //calling substract
+    std::cout<<"calling add 2 * 3  ="<< compute(2 , 3 , '4')<<std::endl;  //calling multiply
+
+    return 0;
+}
+
