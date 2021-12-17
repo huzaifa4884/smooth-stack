0a9b62e9b5fe60961675dc242b7c497b7e5b0cb1
huzaifa4884 <huzaifa.af@gmail.com>
2021-12-17 15:26:36 -0500

    reading single bits now

--- week2/day3/funptrs.cpp
+++ week2/day3/funptrs.cpp
@@ -4,6 +4,11 @@
 
 int (*funpt[3])(int , int);
 
+bool getBit(unsigned char byte, int position) // position in range 0-7
+{
+    return (byte >> position) & 0x1;
+}
+
 int add(int x , int y)
 {
     return x + y;
@@ -19,25 +24,30 @@ int multiply(int x , int y)
     return x * y;
 }
 
-int compute(int x , int y , char funcmask)
+int compute(int x , int y , unsigned char funcmask)
 {
     funpt[0] = &add;
     funpt[1] = &substract;
     funpt[2] = &multiply;
-    
-    unsigned op = (unsigned) funcmask & 7;
-    switch(op)
+
+    bool b1 = getBit(funcmask , 0);
+    bool b2 = getBit(funcmask , 1);
+    bool b3 = getBit(funcmask , 2);
+
+    if (b1)
     {
-        case 1:
-            return (*funpt[0])(x , y);
-        case 2:
-            return (*funpt[1])(x , y);
-        case 4:
-            return (*funpt[2])(x , y);
-        default:
-            std::cout<<"no valid selection made";
-            return 0;
+        return (*funpt[0])(x , y);
     }
+    else if (b2)
+    {
+        return (*funpt[1])(x , y);
+    }
+    else if (b3)
+    {
+        return (*funpt[2])(x , y);
+    }
+
+    return 0;
 }
 
 int main()
