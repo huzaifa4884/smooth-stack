796e897044498fe3e1cc204d951aede1ffa6e7b3
huzaifa4884 <huzaifa.af@gmail.com>
2022-07-25 00:46:58 -0400

    tweaks

--- java/jdass/src/test/java/com/smoothstack/jdass/JdassApplicationTests.java
+++ java/jdass/src/test/java/com/smoothstack/jdass/JdassApplicationTests.java
@@ -1,5 +1,6 @@
 package com.smoothstack.jdass;
 
+import com.smoothstack.jdass.Controller.StudentController;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +9,15 @@ class JdassApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+	@Test
+	void requestForStudent() {
+		StudentController studentController = new StudentController();
+		Student st = new Student();
+		st.setName("huzaifa");
+		st.setId(0);
+		st.setSchoolId(0);
+		assert(studentController.func(0).equals(st));
 	}
 
 }
